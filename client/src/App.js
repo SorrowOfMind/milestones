9002f1b96f73a02b06e4133dafbda3e0f6214d30
SorrowOfMind <sortoflifecraze@gmail.com>
2020-12-26 11:34:00 +0100

    map through posts

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const classes = useStyles();
   const dispatch = useDispatch();
 
-  useEffect(() => dispatch(getPosts), [dispatch]);
+  useEffect(() => dispatch(getPosts()), [dispatch]);
 
   return (
     <Container maxwidth="lg">
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,7 +3,6 @@ const Post = require('../models/post');
 const getPosts = async (req, res) => {
     try {
         const posts = await Post.find();
-        console.log(posts);
         res.status(200).json(posts);
     } catch (error) {
         res.status(404).json({message: error.message})
